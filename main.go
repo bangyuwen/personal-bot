8df0b6a2511671faff6da7fd8bec8c7511d5eed7
bangyuwen <bangyuwen@gmail.com>
2022-03-01 23:22:59 +0800

    init AAII

--- main.go
+++ main.go
@@ -10,7 +10,50 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-func parse(e *colly.HTMLElement) string {
+var lastMsgFG = ""
+var lastMsgAAII = ""
+
+func sendMessage(msg string) {
+	reqUrl := "https://api.telegram.org/bot1616062145:AAEw9aiOA5Jgo2bjJv7C6iTnUkD3Uu0pMQs/sendMessage?chat_id=-1001500464600&text="
+	resp, err := http.Get(reqUrl + url.QueryEscape(msg))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp)
+}
+
+func parseAAII(e *colly.HTMLElement) string {
+	msg := []string{"AAII"}
+	e.ForEach("li", func(_ int, el *colly.HTMLElement) {
+		name := el.ChildText(".stat-name")
+		date := el.ChildText(".date-label")
+		fmt.Println(date)
+		val := el.ChildText(".stat-val .val")
+		msg = append(msg, name+" "+date+" "+val+"%")
+	})
+	return strings.Join(msg, "\n")
+}
+
+func crawlAAII() {
+	c := colly.NewCollector()
+	responseMsg := ""
+
+	c.OnHTML(".sidebar-box-block.chart-stat-lastrows", func(e *colly.HTMLElement) {
+		responseMsg = parseAAII(e)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://www.macromicro.me/charts/20828/us-aaii-sentimentsurvey")
+	if responseMsg != lastMsgAAII {
+		sendMessage(responseMsg)
+		lastMsgAAII = responseMsg
+	}
+}
+
+func parseFG(e *colly.HTMLElement) string {
 	msg := []string{"CNN Fear & Greed Index"}
 	updated_at := e.ChildText("#needleAsOfDate")
 	e.ForEach("div#needleChart li", func(_ int, el *colly.HTMLElement) {
@@ -20,12 +63,12 @@ func parse(e *colly.HTMLElement) string {
 	return strings.Join(msg, "\n")
 }
 
-func crawl() {
+func crawlFG() {
 	c := colly.NewCollector()
 	responseMsg := ""
 
 	c.OnHTML("div.modContent.feargreed", func(e *colly.HTMLElement) {
-		responseMsg = parse(e)
+		responseMsg = parseFG(e)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -33,18 +76,16 @@ func crawl() {
 	})
 
 	c.Visit("https://money.cnn.com/data/fear-and-greed/")
-	reqUrl := "https://api.telegram.org/bot1616062145:AAEw9aiOA5Jgo2bjJv7C6iTnUkD3Uu0pMQs/sendMessage?chat_id=-1001500464600&text="
-	resp, err := http.Get(reqUrl + url.QueryEscape(responseMsg))
-	if err != nil {
-		fmt.Println(err)
+	if responseMsg != lastMsgFG {
+		sendMessage(responseMsg)
+		lastMsgFG = responseMsg
 	}
-	fmt.Println(resp)
 }
 
 func main() {
-	// crawl()
 	c := cron.New()
-	c.AddFunc("@hourly", crawl)
+	c.AddFunc("@hourly", crawlFG)
+	c.AddFunc("@hourly", crawlAAII)
 	fmt.Println("Start Cron Crawl")
 	fmt.Println(c.Entries())
 	c.Start()
